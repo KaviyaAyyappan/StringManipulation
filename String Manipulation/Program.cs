12d9922d3593bb5cda1b29908d7c50a85ba8c0c2
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-05-14 11:15:35 +0200

    String manipulation

---
+++ String Manipulation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the string:");
+            string name = Console.ReadLine();
+
+            string underscores = name.Replace(" ", "_");
+            string leadingSpace = name.TrimStart();
+            string trailingSpace = name.TrimEnd();
+            string upperCase = name.Replace("a", "A");
+
+            Console.WriteLine("The space into _:" +underscores);
+            Console.WriteLine("The a into A:" +upperCase);
+            Console.WriteLine("Remove Leading spaces:" + leadingSpace);
+            Console.WriteLine("Remove Trailing spaces:" + trailingSpace);
+            Console.ReadLine();
+
+            //Replaces all lowercase  by uppercase
+            //string upperCase = string.Empty;
+            //for(int i=0;i<name.Length;i++)
+            //{
+            //   if(name[i]>='a' && name[i]<='z')
+            //    {
+            //        upperCase += (char)(name[i] - 'a' + 'A');
+            //    }
+            //    else
+            //    {
+            //        upperCase += name[i];
+            //    }
+            //}
+            //Console.WriteLine(upperCase);
+
+        }
+
+    }
+    }
